8ca009b79481874b230e806fcf20dd268f112956
cammip <cammip@uw.edu>
2024-02-22 19:50:37 -0800

    update

--- app/src/main/java/edu/uw/ischool/cammip/awty/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/cammip/awty/MainActivity.kt
@@ -1,21 +1,19 @@
 package edu.uw.ischool.cammip.awty
 
-import android.app.Activity
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.text.TextUtils
 import android.widget.Toast
-
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.SystemClock
+import android.content.IntentFilter
 import android.util.Log
-import kotlin.coroutines.coroutineContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var button: Button
@@ -23,7 +21,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var num: EditText
     private lateinit var nag: EditText
 
+    @SuppressLint("UnspecifiedRegisterReceiverFlag")
     override fun onCreate(savedInstanceState: Bundle?) {
+        var alarmReceiver: BroadcastReceiver? = null
+
+        Log.d("Test", "started")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -31,20 +33,17 @@ class MainActivity : AppCompatActivity() {
         message = findViewById(R.id.edit1)
         num = findViewById(R.id.edit2)
         nag = findViewById(R.id.edit3)
+        val ALARM_ACTION = "edu.uw.ischool.cammip.ALARM"
 
-        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val intent = Intent(this, MyAlarm::class.java)
-        val pendingIntent = PendingIntent.getBroadcast(this, 0,
-            intent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-        button.setOnClickListener() {
+        button.setOnClickListener {
             //end alarm
             if(button.text.toString() == "Stop") {
-                button.text = "Start"
-
-                alarmManager.cancel(pendingIntent);
+                unregisterReceiver(alarmReceiver)
+                alarmReceiver = null
                 Toast.makeText(this, "Alarm stopped", Toast.LENGTH_SHORT).show()
+                button.text = "Start"
             }
+
             //if the data is invalid, button does nothing
             else if(button.text.toString() == "Start" &&
                 TextUtils.isEmpty(message.text.toString())
@@ -57,29 +56,32 @@ class MainActivity : AppCompatActivity() {
             //if data is valid, start alarm
             } else if (button.text.toString() != "Stop"){
                 button.text = "Stop"
+                val alarmManager = applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
-                sendBroadcast(intent)
+                val intent = Intent(ALARM_ACTION)
+                val pendingIntent = PendingIntent.getBroadcast(applicationContext,
+                    0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+                if(alarmReceiver == null) {
+                    alarmReceiver = object : BroadcastReceiver() {
+                        override fun onReceive(context: Context?, intent: Intent?) {
+                            Log.d("Test", "onReceive called")
+                            val msg = "${num.text.toString()}: ${message.text.toString()}"
+                            Toast.makeText(context, msg, Toast.LENGTH_LONG).show()
+                            Log.d("Test", "toast was toasted")
+
+                        }
+                    }
+                    val filter = IntentFilter(ALARM_ACTION)
+                    registerReceiver(alarmReceiver, filter)
+                }
 
                 alarmManager.setRepeating(
-                    AlarmManager.ELAPSED_REALTIME,
-                    SystemClock.elapsedRealtime(),
-                    nag.text.toString().toLong() * 60000,
-                    pendingIntent
-                )
+                    AlarmManager.RTC_WAKEUP,
+                    System.currentTimeMillis() + num.text.toString().toInt(),
+                    num.text.toString().toLong() * 60000,
+                    pendingIntent)
             }
-        }
-    }
-
-    class MyAlarm : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            val num = context.applicationContext.findViewById<EditText>(R.id.edit2)
-            //val msg = (context as Activity).findViewById<EditText>(R.id.edit1)
-            //val message = "${num.text.toString()}: ${msg.text.toString()}"
-
-            val message = num.toString()
-            Log.d("msg", message)
-            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-
         }
     }
 }
